cc7c1984fbf3de67b954e5534f5c89162dade9e5
SharozTanveer <saadtanveer7@live.co.uk>
2020-04-08 01:24:47 +0500

    fix download

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 const download = require('download-git-repo')
 const csv = require('csvtojson')
-const fs = require("fs")
 const csvFilePath = __dirname + '/data/covid-19-pakistan-data.csv'
 const dataDir = __dirname + '/data'
 const gitRepo = 'ShahrozTanveer/covid-19-pakistan'
-let updated=0
 async function csvToJson(){ 
     const jsonArray = await csv().fromFile(csvFilePath);
     return jsonArray
 }
-
-
-
 function downloadData(){
-    download(gitRepo, dataDir, (err) => {
+     download(gitRepo, dataDir, (err) => {
         if (err) {
             console.log(err)
           
         } 
+        else{
+            console.log("updated");
+        }
 
 
     })
+   
+
 
 }
 async function getData(){
     const data=await csvToJson();
-    
+    console.log(data);
     return data
 }
-downloadData()
-
-getData()
 
 
 module.exports={
     about : "this is covid19-pakistan package",
     getData:getData,
-    updateData:downloadData
+    downloadData:downloadData
+   
 }
